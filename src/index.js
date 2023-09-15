3e8d8f386f64ba81b63c86454caa56e0cf4da54a
y-hrubskyi <y.hrubskyi@gmail.com>
2023-09-16 02:24:16 +0300

    without axios & async/await

---
+++ src/index.js
@@ -0,0 +1,122 @@
+import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const API_KEY = '39469696-20028cb0579d07044da7a8037';
+const BASE_URL = 'https://pixabay.com/api';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+const per_page = 40;
+let page = 1;
+let searchQuery = '';
+let lightbox = null;
+
+refs.searchForm.addEventListener('submit', onSearchFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+
+function onSearchFormSubmit(event) {
+  event.preventDefault();
+  refs.galleryContainer.innerHTML = '';
+  refs.loadMoreBtn.classList.add('visually-hidden');
+  page = 1;
+
+  searchQuery = event.currentTarget.elements.searchQuery.value;
+
+  fetchSearchResults(searchQuery)
+    .then(results => {
+      if (!results.hits.length) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+          // { position: 'center-center' }
+        );
+        return;
+      }
+
+      Notify.info(`Hooray! We found ${results.totalHits} images.`);
+      renderSearchResults(results.hits);
+      lightbox = new SimpleLightbox('.gallery a');
+
+      if (results.hits.length < per_page) {
+        return;
+      }
+      refs.loadMoreBtn.classList.remove('visually-hidden');
+    })
+    .catch(console.log);
+}
+
+function onLoadMoreBtnClick() {
+  page += 1;
+  fetchSearchResults(searchQuery)
+    .then(results => {
+      renderSearchResults(results.hits);
+
+      lightbox.refresh();
+
+      if (results.totalHits <= page * per_page) {
+        refs.loadMoreBtn.classList.add('visually-hidden');
+        Notify.warning(
+          "We're sorry, but you've reached the end of search results.",
+          { position: 'right-bottom' }
+        );
+      }
+    })
+    .catch(console.log);
+}
+
+function createSearchResultMarkup(item) {
+  return `<div class="photo-card">
+        <a href="${item.largeImageURL}">
+          <div class="thumb">
+              <img src="${item.webformatURL}" alt="${item.tag} loading="lazy" />
+          </div>
+        </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            <span class="stats-quantity">${item.likes}</span>
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            <span class="stats-quantity">${item.views}</span>
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            <span class="stats-quantity">${item.comments}</span>
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            <span class="stats-quantity">${item.downloads}</span>
+          </p>
+        </div>
+      </div>`;
+}
+
+function renderSearchResults(array) {
+  const markup = array.map(createSearchResultMarkup).join('');
+  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+function fetchSearchResults(searchQuery) {
+  const searchParams = new URLSearchParams({
+    key: API_KEY,
+    q: searchQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    per_page,
+    page,
+  });
+  const url = `${BASE_URL}/?${searchParams}`;
+
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
